139f114e2d16c75a741f3aed5d7246715ef3845b
karthik156kk <karthik582tn@gmail.com>
2024-02-02 18:38:57 +0530

    grid snapping

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Chessboard from './components/Chessboard';
+// import LeftPanel from './components/LeftPanel';
+// import RightPanel from './components/RightPanel';
 function App() {
   return (
     <div className="App">
+        {/* <LeftPanel /> */}
         <Chessboard />
+        {/* <RightPanel /> */}
     </div>
   );
 }
--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import Tile from './Tile';
 import './Chessboard.css';
+import {useRef, useEffect, useState} from 'react';
+
+const initialSetup = [];
+for(let p=0; p<2; p++){
+    const type = p===0 ? 'w' : 'b';
+    const y = p === 0 ? 1 : 8;
+
+    initialSetup.push({image: `/assets/images/rook_${type}.png`, x:1, y});
+    initialSetup.push({image: `/assets/images/rook_${type}.png`, x:8, y});
+    initialSetup.push({image: `/assets/images/knight_${type}.png`, x:2, y});
+    initialSetup.push({image: `/assets/images/knight_${type}.png`, x:7, y});
+    initialSetup.push({image: `/assets/images/bishop_${type}.png`, x:3, y});
+    initialSetup.push({image: `/assets/images/bishop_${type}.png`, x:6, y});
+    initialSetup.push({image: `/assets/images/queen_${type}.png`, x:4, y});
+    initialSetup.push({image: `/assets/images/king_${type}.png`, x:5, y});
+}
+for(let k=1; k<=8; k++){
+    initialSetup.push({image: '/assets/images/pawn_b.png', x:k, y:7});
+    initialSetup.push({image: '/assets/images/pawn_w.png', x:k, y:2});
+}
+
 function Chessboard() {
+    const forReference = useRef(null);
+    const [pieces, setPieces] = useState(initialSetup);
+    const [activePiece, setActivePiece] = useState(null);
+    const [gridX, setGridX] = useState(0);
+    const [gridY, setGridY] = useState(0);
+
     const horizontalAxis = ['a','b','c','d','e','f','g','h'];
     const verticalAxis = ['1', '2','3','4','5','6','7','8'];
     let board = [];
 
-    const pieces = [];
-    for(let p=0; p<2; p++){
-        const type = p===0 ? 'w' : 'b';
-        const y = p === 0 ? 1 : 8;
-
-        pieces.push({image: `/assets/images/rook_${type}.png`, x:1, y});
-        pieces.push({image: `/assets/images/rook_${type}.png`, x:8, y});
-        pieces.push({image: `/assets/images/knight_${type}.png`, x:2, y});
-        pieces.push({image: `/assets/images/knight_${type}.png`, x:7, y});
-        pieces.push({image: `/assets/images/bishop_${type}.png`, x:3, y});
-        pieces.push({image: `/assets/images/bishop_${type}.png`, x:6, y});
-        pieces.push({image: `/assets/images/queen_${type}.png`, x:4, y});
-        pieces.push({image: `/assets/images/king_${type}.png`, x:5, y});
-    }
-    for(let k=1; k<=8; k++){
-        pieces.push({image: '/assets/images/pawn_b.png', x:k, y:7});
-        pieces.push({image: '/assets/images/pawn_w.png', x:k, y:2});
-    }
     for(let i=verticalAxis.length-1; i>=0; i--){
         for(let j=0; j<horizontalAxis.length; j++){
             const flag = i+j;
@@ -37,34 +46,72 @@ function Chessboard() {
             board.push(<Tile num={key} flag={flag} image={pieceImg}/>);
         }
     }
-    let activePiece = null;
     const grabElement = (e)=>{
         const element = e.target;
-        if(element.classList.contains('chessPiece')){
-            const x = e.clientX - 50;
-            const y = e.clientY -50;
-            element.style.position = 'absolute';
-            element.style.left = `${x}px`;
-            element.style.top = `${y}px`;
-            activePiece = element;
+        const currentBoard = forReference.current;
+        if(element.classList.contains('chessPiece') && currentBoard){
+            const gX = Math.floor((e.clientX - currentBoard.offsetLeft)/75) + 1;
+            const gY = Math.abs(Math.ceil((e.clientY - currentBoard.offsetTop - 600)/75) - 1);
+            setGridX(gX);
+            setGridY(gY);
+            //redundant
+            // const x = e.clientX - 50;
+            // const y = e.clientY -50;
+            // element.style.position = 'absolute';
+            // element.style.left = `${x}px`;
+            // element.style.top = `${y}px`;
+            setActivePiece(element);
         }
     }
     const moveElement = (e)=>{
-        if(activePiece){
+        if(activePiece && forReference){
+            const minX = forReference.current.offsetLeft - 10;
+            const minY = forReference.current.offsetTop - 10;
+            const maxX = forReference.current.clientWidth + minX - 60;
+            const maxY = forReference.current.clientHeight + minY - 60;
             const x = e.clientX - 50;
             const y = e.clientY -50;
             activePiece.style.position = 'absolute';
-            activePiece.style.left = `${x}px`;
-            activePiece.style.top = `${y}px`;
+            if(x < minX){
+                activePiece.style.left = `${minX}px`;
+            }else if(x > maxX){
+                activePiece.style.left = `${maxX}px`;
+            }else{
+                activePiece.style.left = `${x}px`;
+            }
+            if(y < minY){
+                activePiece.style.top = `${minY}px`;
+            }else if(y > maxY){
+                activePiece.style.top = `${maxY}px`;
+            }else{
+                activePiece.style.top = `${y}px`;
+            }
+        
         }
     }
     const dropElement = (e)=>{
+        const currentBoard = forReference.current;
         if(activePiece){
-            activePiece = null;
+            const x = Math.floor((e.clientX - currentBoard.offsetLeft)/75) + 1;
+            const y = Math.abs(Math.ceil((e.clientY - currentBoard.offsetTop - 600)/75) - 1);
+            console.log(`${x},${y} to ${gridX},${gridY}`);
+            setPieces((prevPieces)=>{
+                const newPieces = prevPieces.map((p)=>{
+                    if(p.x ===gridX && p.y===gridY){
+                        p.x = x;
+                        p.y = y;
+                    }
+                    return p;
+                })
+                return newPieces;
+            })
+            console.log(pieces)
+            setActivePiece(null);
         }
+        
     }
     return(
-        <div className="backboard" onMouseDown={grabElement} onMouseMove={moveElement} onMouseUp={dropElement}> 
+        <div className="backboard" onMouseDown={grabElement} onMouseMove={moveElement} onMouseUp={dropElement} ref={forReference}> 
             {board}
         </div>
     )
