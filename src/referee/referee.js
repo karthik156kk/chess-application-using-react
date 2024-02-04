f57d4a46c9f00821263a133d193f28f306bd702c
karthik156kk <karthik582tn@gmail.com>
2024-02-04 15:43:10 +0530

    knight movement

--- src/referee/referee.js
+++ src/referee/referee.js
@@ -1,22 +1,26 @@
 import {pieceType, pieceTeam} from '../components/constants';
 export default class Referee{
-    isTileOccupied(x,y,boardState){
-        const findOccupiedPiece = boardState.find((p) => p.position.x===x && p.position.y===y);
+
+    isTileOccupied(tilePosition,boardState){
+        const findOccupiedPiece = boardState.find((p) => p.position.x===tilePosition.x && p.position.y===tilePosition.y);
         if(findOccupiedPiece){
             return true;
         }
         return false;
     }
 
-    isTileOccupiedByOpponent(x,y, boardState, playerTeam){
-        const opponent = boardState.find((p) => p.position.x===x && p.position.y===y && p.team !== playerTeam);
+    isTileOccupiedByOpponent(tilePosition, boardState, playerTeam){
+        const opponent = boardState.find((p) => p.position.x===tilePosition.x && p.position.y===tilePosition.y && p.team !== playerTeam);
         if(opponent){
             return true;
         } else{
             return false;
         }
     }
-
+    isTileEmptyOrEnemyOccupied(desiredPosition, boardState, playerTeam){
+        return !this.isTileOccupied(desiredPosition, boardState) || this.isTileOccupiedByOpponent(desiredPosition, boardState, playerTeam)
+    }
+    
     isEnpassantMove(initialPosition, desiredPosition , playerType, playerTeam, boardState){
         const pawnDirection = playerTeam===pieceTeam.OUR ? 1 : -1;
         if(playerType === pieceType.PAWN){
@@ -36,25 +40,39 @@ export default class Referee{
             const pawnDirection = (team===pieceTeam.OUR) ? 1 : -1;
             //Movement logic
             if(initialPosition.y===specialRow && desiredPosition.x===initialPosition.x && desiredPosition.y-initialPosition.y===2*pawnDirection){
-                if(!this.isTileOccupied(desiredPosition.x, desiredPosition.y, currBoardState) && !this.isTileOccupied(desiredPosition.x, desiredPosition.y-pawnDirection, currBoardState)){
+                if(!this.isTileOccupied(desiredPosition, currBoardState) && !this.isTileOccupied({x:desiredPosition.x, y:desiredPosition.y-pawnDirection}, currBoardState)){
                     return true;
                 }
             } else if(desiredPosition.x===initialPosition.x && desiredPosition.y-initialPosition.y===pawnDirection){
-                if(!this.isTileOccupied(desiredPosition.x, desiredPosition.y, currBoardState)){
+                if(!this.isTileOccupied(desiredPosition, currBoardState)){
                     return true;
                 }
             }
             //Attack logic
             else if(desiredPosition.x-initialPosition.x===-1 && desiredPosition.y-initialPosition.y===pawnDirection){
-                if(this.isTileOccupiedByOpponent(desiredPosition.x, desiredPosition.y, currBoardState, team)){
+                if(this.isTileOccupiedByOpponent(desiredPosition, currBoardState, team)){
                     return true;
                 }
             } else if(desiredPosition.x-initialPosition.x===1 && desiredPosition.y-initialPosition.y===pawnDirection){
-                if(this.isTileOccupiedByOpponent(desiredPosition.x, desiredPosition.y, currBoardState, team)){
+                if(this.isTileOccupiedByOpponent(desiredPosition, currBoardState, team)){
                     return true;
                 }
             }
-        } 
+        } else if(type === pieceType.KNIGHT){
+            for(let i=-1; i<2; i+=2){
+                for(let j=-1; j<2; j+=2){
+                    if(desiredPosition.x - initialPosition.x === 2*i && desiredPosition.y - initialPosition.y === j){
+                        if(this.isTileEmptyOrEnemyOccupied(desiredPosition, currBoardState, team)){
+                            return true;
+                        }
+                    } else if(desiredPosition.x - initialPosition.x === i && desiredPosition.y - initialPosition.y === 2*j){
+                        if(this.isTileEmptyOrEnemyOccupied(desiredPosition, currBoardState, team)){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 }
